13339d63a8538a6db4adc0c2203805e88b08bebc
Brunogodoy2911 <brunogodoy29@hotmail.com>
2025-10-31 17:37:56 -0300

    :sparkles: Metodo DELETE criado com sucesso

--- app.py
+++ app.py
@@ -36,6 +36,7 @@ def get_task(id):
   for t in tasks:
     if t.id == id:
       return jsonify(t.to_dict())
+    
   return jsonify({ "message": "Não foi possível encontrar a atividade"}), 404
 
 @app.route("/tasks/<int:id>", methods=["PUT"])
@@ -44,6 +45,7 @@ def update_task(id):
   for t in tasks:
     if t.id == id:
       task = t
+      break
   
   if task == None:
     return jsonify({ "message": "Não foi possível encontrar a atividade"}), 404
@@ -53,6 +55,21 @@ def update_task(id):
   task.description = data["description"]
   task.completed = data["completed"]
   return jsonify({"message": "Tarefa atualizada com sucesso!"})
+
+@app.route("/tasks/<int:id>", methods=["DELETE"])
+def delete_task(id):
+  task = None
+  for t in tasks:
+    if t.id == id:
+      task = t
+      break
+
+  if not task:
+    return jsonify({ "message": "Não foi possível encontrar a atividade"}), 404
+  
+  tasks.remove(task)
+  return jsonify({ "message": "Tarefa deletada com sucesso!"})
+
 
 if __name__ == "__main__":
   app.run(debug=True)
